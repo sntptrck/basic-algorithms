409b1e14cc168954731024fe0a50537fc9813320
Пигалов Дмитрий Сергеевич <ds_pigalov@bbgroup.local>
2021-02-10 16:40:12 +0300

    update

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    "^.+\\.jsx?$": require.resolve("babel-jest"),
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testPathIgnorePatterns: ["/build/", "/node_modules/"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverage: true
-};
